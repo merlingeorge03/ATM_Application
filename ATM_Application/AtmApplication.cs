1feb509bba24b3408c33ae16a8a5d48a689dcd0f
Merlin George <Mgeorge9980@conestogac.on.ca>
2024-06-15 00:39:08 -0400

    Add project files.

---
+++ ATM_Application/AtmApplication.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ATM_Application
+{
+    internal class AtmApplication
+    {
+        public void displayMainMenu()
+        {
+            int choice;
+            Console.WriteLine("========================================= WELCOME TO ATM APPLICATION =========================================");
+            do
+            {
+                Console.WriteLine("============= ATM MAIN MENU =============");
+                Console.WriteLine("1. Create Account");
+                Console.WriteLine("2. Select Account");
+                Console.WriteLine("3. Exit Application");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        break;
+
+                }
+            } while (choice != 3);
+        }
+    }
+}
---
+++ ATM_Application/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ATM_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AtmApplication app = new AtmApplication();
+            app.displayMainMenu();
+        }
+    }
+}
