e2869d8a265a822825bc00f063c85009019be233
Merlin George <Mgeorge9980@conestogac.on.ca>
2024-06-15 07:27:08 -0400

    * Created method for selectingAccount *Created method for displaying account menu

--- ATM_Application/AtmApplication.cs
+++ ATM_Application/AtmApplication.cs
@@ -8,7 +8,7 @@ namespace ATM_Application
         {
             int choice;
             string exitChoice;
-            
+
             do
             {
                 Console.WriteLine("============= ATM MAIN MENU =============");
@@ -20,7 +20,7 @@ namespace ATM_Application
                 switch (choice)
                 {
                     case 1:
-                        //createAccount
+                        createAccount();
                         break;
                     case 2:
                         //selectAccount
@@ -33,15 +33,14 @@ namespace ATM_Application
                         {
                             Console.WriteLine("THANK YOU FOR CHOOSING THE ATM APPLICATION !!");
                         }
-                        else 
+                        else
                         {
                             if (exitChoice != "n")
                             {
                                 Console.WriteLine("Press y to exit and n to continue operations");
-                              
+
                             }
-                            displayMainMenu();     
-                        
+                            displayMainMenu();
                         }
                         break;
                     default:
@@ -54,11 +53,80 @@ namespace ATM_Application
 
         public void createAccount()
         {
+            Console.WriteLine("====== 1. CREATE ACCOUNT MENU =====");
+            Console.WriteLine("Enter Account Holder Name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Account Number (Account number must be between 100 and 1000)");
+            int accountNumber = int.Parse(Console.ReadLine());
+
+            //Default accounts already created from 100 to 110. 
+            //Check if the user entered the same account number as default account number
+            if (accountNumber >= 100 && accountNumber <= 110)
+            {
+                Console.WriteLine("This Account Number already exists in the database. Please enter another number");
+                Console.WriteLine("Enter Account Number (Account number must be between 110 and 1000)");
+                accountNumber = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter Annual Interest Rate (Must be less than 3.0%)");
+            float interestRate = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Initial Balance");
+            float initialBalance = float.Parse(Console.ReadLine());
+
+            // Account class to be called and feed the data
+
+            Console.WriteLine("Account created successfully.");
 
         }
         public void selectAccount()
         {
+            Console.WriteLine("====== 2. SELECT ACCOUNT =======");
+            Console.WriteLine("Enter Account Number (Account number must be between 100 and 1000)");
+            int accountNumber = int.Parse(Console.ReadLine());
 
+            //if account selected is not null (exists), then do the below actions
+            //welcome message to be printed along with the name
+            //displayAccountMenu();
+        }
+
+        private void DisplayAccountMenu()
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("------- ACCOUNT MENU -------");
+                Console.WriteLine("Choose from the following options.");
+                Console.WriteLine("1. Check Balance");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                Console.WriteLine("4. Display Transaction");
+                Console.WriteLine("5. Exit Account");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        //CheckBalance method to be called
+                        break;
+                    case 2:
+                        //Deposit method to be called 
+                        break;
+                    case 3:
+                        //Withdraw method to be called 
+                        break;
+                    case 4:
+                        //DisplayTransactions method to be called 
+                        break;
+                    case 5:
+                        Console.WriteLine("Exiting Account Menu...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+            } while (choice != 5);
         }
     }
 }
